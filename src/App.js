d91c6bafd1b2f91160027eea0b86da3503a8d4ba
MatildaHJ <matilda.hj.larsson@gmail.com>
2024-03-07 16:55:37 +0100

    installed bootstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>Hello</h1>
+      </div>
+    </div>
+  );
 }
